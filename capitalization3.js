10858220f4f5b674bd96003d702d6ac7ca4e1256
Graeme Clark <grae0026@stud.kea.dk>
2020-02-10 20:58:28 +0100

    Add first part and middle part of name variables

--- capitalization3.js
+++ capitalization3.js
@@ -1,1 +1,19 @@
 "use strict";
+
+console.log("hello");
+
+const firstName = "GRAEME";
+const newString = firstName.toLowerCase();
+const firstPart = firstName.substring(0, 3);
+const middlePart = firstName.substring(3);
+
+console.log(newString);
+console.log(firstPart);
+console.log(middlePart);
+
+// const fullName = "Dave Bloody Brubeck";
+// const firstSpace = fullName.indexOf(" ");
+// const secondSpace = fullName.indexOf(" ", firstSpace + 1);
+// const firstName = fullName.substring(0, firstSpace);
+// const middleName = fullName.substring(firstSpace + 1, secondSpace);
+// const lastName = fullName.substring(secondSpace + 1);

7136ff4934c7fb69aa5aab20f56bcb30648bfbd8
Graeme Clark <grae0026@stud.kea.dk>
2020-02-10 21:28:33 +0100

    Adjusted variable names

--- capitalization3.js
+++ capitalization3.js
@@ -1,15 +1,18 @@
 "use strict";
 
-console.log("hello");
+const firstName = "Alexander";
 
-const firstName = "GRAEME";
-const newString = firstName.toLowerCase();
-const firstPart = firstName.substring(0, 3);
-const middlePart = firstName.substring(3);
+const nameLowerCase = firstName.toLowerCase();
+const firstPart = nameLowerCase.substring(0, 2);
+const thirdLetter = nameLowerCase.toUpperCase().substring(3, 2);
+const endPart = nameLowerCase.substring(3);
 
-console.log(newString);
+console.log(nameLowerCase);
 console.log(firstPart);
-console.log(middlePart);
+console.log(thirdLetter);
+console.log(endPart);
+
+console.log(`Your name is : ${firstPart}${thirdLetter}${endPart}`);
 
 // const fullName = "Dave Bloody Brubeck";
 // const firstSpace = fullName.indexOf(" ");

c06af9ca9611f48223ba2f752a5d02da3a2d2da3
Graeme Clark <grae0026@stud.kea.dk>
2020-02-10 22:17:48 +0100

    Add newFirstName variable

--- capitalization3.js
+++ capitalization3.js
@@ -6,10 +6,8 @@ const nameLowerCase = firstName.toLowerCase();
 const firstPart = nameLowerCase.substring(0, 2);
 const thirdLetter = nameLowerCase.toUpperCase().substring(3, 2);
 const endPart = nameLowerCase.substring(3);
+const newFirstName = firstPart + thirdLetter + endPart;
 
-console.log(nameLowerCase);
-console.log(firstPart);
-console.log(thirdLetter);
-console.log(endPart);
+document.querySelector("h2").textContent = newFirstName;
 
 console.log(`Your name is : ${firstPart}${thirdLetter}${endPart}`);
